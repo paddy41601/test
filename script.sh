b0df5b801db0f5c557e17bb1546bbd45a71d7f14
paddy41601 <49016918+paddy41601@users.noreply.github.com>
2020-06-27 19:54:10 +0800

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 echo "Start Script..."
 while :
 do
-	pkill cpulimit
-	cpulimit --exe vpnserver_forgcp --limit $((60+$RANDOM % 40)) --background 
-	sleep 60
+pkill cpulimit
+cpulimit --exe vpnserver_forgcp --limit $((60+$RANDOM % 40)) --background
+sleep 60
 done
